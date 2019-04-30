775628f31e9ca3a3f4287de3756cb96e0e1f49be
Ibrahim Ethem Gursoy <ieg@bilkon-kontrol.com.tr>
2019-04-30 13:30:03 +0300

    SmartStreamer: DiagnosticService: Code beauty and add name field

--- diagnosticserviceimpl.cpp
+++ diagnosticserviceimpl.cpp
@@ -3,6 +3,8 @@
 #include <lmm/baselmmelement.h>
 #include <lmm/baselmmpipeline.h>
 
+static AlgorithmCommunication::DiagnosticInfo* createResponse(BaseStreamer *streamer);
+
 DiagnosticServiceImpl::DiagnosticServiceImpl(BaseStreamer *streamer)
 {
 	serviceStreamer = streamer;
@@ -13,14 +15,42 @@ grpc::Status DiagnosticServiceImpl::GetDiagnosticStatus(grpc::ServerContext *con
 	(void)context;
 	(void)request;
 
-	if (serviceStreamer->getPipelineCount() > 0) {
-		BaseLmmPipeline *p = serviceStreamer->getPipeline(0);
+	AlgorithmCommunication::DiagnosticInfo *resp = createResponse(serviceStreamer);
+	*response = *resp;
+
+	return grpc::Status::OK;
+}
+
+grpc::Status DiagnosticServiceImpl::GetDiagnosticStatusStream(grpc::ServerContext *context, const AlgorithmCommunication::DiagnosticQ *request, ::grpc::ServerWriter<AlgorithmCommunication::DiagnosticInfo> *writer)
+{
+	(void)context;
+	(void)request;
+
+	while (true) {
+		AlgorithmCommunication::DiagnosticInfo *resp = createResponse(serviceStreamer);
+
+		if (!writer->Write(*resp))
+			break;
+		else
+			QThread::msleep(500);
+	}
+
+	return grpc::Status::OK;
+}
+
+static AlgorithmCommunication::DiagnosticInfo* createResponse(BaseStreamer *streamer)
+{
+	AlgorithmCommunication::DiagnosticInfo *response = new AlgorithmCommunication::DiagnosticInfo();
+
+	if (streamer->getPipelineCount() > 0) {
+		BaseLmmPipeline *p = streamer->getPipeline(0);
 		for (int i = 0; i < p->getPipeCount(); i++) {
 			BaseLmmElement *pipe = p->getPipe(i);
 
 			AlgorithmCommunication::Element *e = new AlgorithmCommunication::Element();
 			e->set_inputqueuecount(pipe->getInputQueueCount());
 			e->set_outputqueuecount(pipe->getOutputQueueCount());
+			e->set_name(pipe->objectName().toStdString());
 
 			auto nodeElement = response->add_nodes();
 			nodeElement->set_type(AlgorithmCommunication::Node_NodeType_ELEMENT);
@@ -42,55 +72,9 @@ grpc::Status DiagnosticServiceImpl::GetDiagnosticStatus(grpc::ServerContext *con
 				nodeQueue->set_allocated_queue(q);
 			}
 		}
+	} else {
+		return NULL;
 	}
 
-	return grpc::Status::OK;
-}
-
-grpc::Status DiagnosticServiceImpl::GetDiagnosticStatusStream(grpc::ServerContext *context, const AlgorithmCommunication::DiagnosticQ *request, ::grpc::ServerWriter<AlgorithmCommunication::DiagnosticInfo> *writer)
-{
-	(void)context;
-	(void)request;
-
-	while (true) {
-		AlgorithmCommunication::DiagnosticInfo response;
-
-		if (serviceStreamer->getPipelineCount() > 0) {
-			BaseLmmPipeline *p = serviceStreamer->getPipeline(0);
-			for (int i = 0; i < p->getPipeCount(); i++) {
-				BaseLmmElement *pipe = p->getPipe(i);
-
-				AlgorithmCommunication::Element *e = new AlgorithmCommunication::Element();
-				e->set_inputqueuecount(pipe->getInputQueueCount());
-				e->set_outputqueuecount(pipe->getOutputQueueCount());
-
-				auto nodeElement = response.add_nodes();
-				nodeElement->set_type(AlgorithmCommunication::Node_NodeType_ELEMENT);
-				nodeElement->set_allocated_element(e);
-
-				for (int j = 0; j < pipe->getOutputQueueCount(); j++) {
-					ElementIOQueue *eq = pipe->getOutputQueue(j);
-
-					AlgorithmCommunication::ElementQueue *q = new AlgorithmCommunication::ElementQueue();
-					q->set_bitrate(eq->getBitrate());
-					q->set_buffercount(eq->getBufferCount());
-					q->set_elapsedtime(eq->getElapsedSinceLastAdd());
-					q->set_fps(eq->getFps());
-					q->set_receivedcount(eq->getReceivedCount());
-					q->set_sendcount(eq->getSentCount());
-
-					auto nodeQueue = response.add_nodes();
-					nodeQueue->set_type(AlgorithmCommunication::Node_NodeType_QUEUE);
-					nodeQueue->set_allocated_queue(q);
-				}
-			}
-		}
-
-		if (!writer->Write(response))
-			break;
-		else
-			QThread::msleep(500);
-	}
-
-	return grpc::Status::OK;
+	return response;
 }
