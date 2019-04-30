d29ed45c4bd8fc96de4b5a75e32a2bf11090391c
Kerim Enes YILMAZ <kerimenes@bilkon-kontrol.com.tr>
2019-04-30 18:24:31 +0300

    algorithm: fix asel_pan function definition.

--- algorithmfunctions.h
+++ algorithmfunctions.h
@@ -13,7 +13,8 @@ void asel_direct_track_release();
 void record_video(unsigned char *buf, int size, int width, int height);
 void asel_bypass(unsigned char *buf, int size, int width, int height,int debug_case,int stabilization_case,int privacy_case,int init_bypass,int sensitivity,int VideoFormat);
 void asel_bypass_release();
-void asel_pan(unsigned char *buf, int size, int width, int height,int RGB_case,int record_case,int shadow_case,int ill_norm_case,int debug_case,unsigned char* meta,int tilt_degree, int pan_degree);
+
+void asel_pan(unsigned char *buf, int size, int width, int height,int RGB_case,int shadow_case,int ill_norm_case,int debug_case,unsigned char* meta,int tilt_degree, int pan_degree);
 void asel_pan_release();
 
 void asel_via_base(unsigned char *buf, int size, int width, int height,int VideoFormat,int shadow_case,int ill_norm_case,int debug_case,int stabilization_case,int privacy_case, unsigned char* meta,float pan_tilt_zoom_read[],bool &alarmFlag,int init_viabase,int sensitivity,bool Classification,bool Overlay);
