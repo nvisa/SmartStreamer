d0edf50e86b9dd2e72ae756227c2fb508590651c
Kerim Enes YILMAZ <kerimenes@bilkon-kontrol.com.tr>
2019-04-30 11:29:31 +0300

    algorithm: add minor changes to motion algorithm

--- algorithm/motionalgorithmelement.cpp
+++ algorithm/motionalgorithmelement.cpp
@@ -30,12 +30,13 @@ int MotionAlgorithmElement::processAlgo(const RawBuffer &buf)
 
 	int width = buf.constPars()->videoWidth;
 	int height = buf.constPars()->videoHeight;
-	control.sensitivity = BaseAlgorithmCommon::instance()->getMotionSensitivity();
+	control.sensitivity = BaseAlgorithmCommon::instance()->getSensitivity("motion_detection");
 	control.classification = BaseAlgorithmCommon::instance()->getMotionClassification();
 
 	asel_via_base((uchar*)buf.constData(), width * height, width, height,
 				  v.rgb,v.shadow, v.ill, v.debug, v.stabilization, v.privacy,
-				  control.meta, control.panTiltZoomRead, control.alarmFlag, control.initialize, control.sensitivity, control.classification, true);
+				  control.meta, control.panTiltZoomRead, control.alarmFlag,
+				  control.initialize, control.sensitivity, control.classification, true);
 	if (control.initialize)
 		control.initialize = 0;
 
@@ -47,15 +48,14 @@ int MotionAlgorithmElement::processAlgo(const RawBuffer &buf)
 
 int MotionAlgorithmElement::baseAlgorithmProcess(const RawBuffer &buf)
 {
-	mDebug("Process in %d", algoState);
+	mInfo("Buffer sending with size %d", algoState);
 	switch (algoState) {
 	case INIT:
 		init();
 		algoState = PROCESS;
 		break;
 	case PROCESS:
-		processAlgo(buf);
-		return 0;
+		return processAlgo(buf);
 		break;
 	case STOPALGO:
 		mDebug("Stopping Algorithm");
