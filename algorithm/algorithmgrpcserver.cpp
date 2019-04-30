2515ec325d3f2db3c461ec58b381daca9836cde3
Kerim Enes YILMAZ <kerimenes@bilkon-kontrol.com.tr>
2019-04-30 11:30:10 +0300

    algorithmgrpcserver: add multiple changes.
    - add stabilization algorithm.
    - fix sensitivity changes.

--- algorithm/algorithmgrpcserver.cpp
+++ algorithm/algorithmgrpcserver.cpp
@@ -55,34 +55,41 @@ AlgorithmGrpcServer::AlgorithmGrpcServer(QObject *parent)
 	: aw::AlgorithmWorks::Service()
 {
 	motionEl = NULL;
+	stabilizationEl = NULL;
 	GrpcThreadAlg *grpcServ = new GrpcThreadAlg(50059, this);
 	grpcServ->start();
 }
 
 grpc::Status AlgorithmGrpcServer::RunMotion(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response)
 {
-	if (motionEl)
+	if (motionEl) {
 		motionEl->setState(BaseAlgorithmElement::INIT);
-	response->set_response(aw::RunStopResponse::SUCCESS);
+		response->set_response(aw::RunStopResponse::SUCCESS);
+	} else
+		response->set_response(aw::RunStopResponse::FAIL);
 	return grpc::Status::OK;
 }
 
 grpc::Status AlgorithmGrpcServer::StopMotion(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response)
 {
-	if (motionEl)
-		motionEl->setState(BaseAlgorithmElement::STOPALGO);
-	response->set_response(aw::RunStopResponse::SUCCESS);
+	if (motionEl) {
+		if (motionEl->getState() == BaseAlgorithmElement::STOPALGO)
+			motionEl->setState(BaseAlgorithmElement::PROCESS);
+		else
+			motionEl->setState(BaseAlgorithmElement::STOPALGO);
+		response->set_response(aw::RunStopResponse::SUCCESS);
+	} else
+		response->set_response(aw::RunStopResponse::FAIL);
 	return grpc::Status::OK;
 }
 
 grpc::Status AlgorithmGrpcServer::ReleaseMotion(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response)
 {
-	if (!motionEl) {
+	if (motionEl) {
+		motionEl->setState(BaseAlgorithmElement::RELEASE);
+		response->set_response(aw::RunStopResponse::SUCCESS);
+	} else
 		response->set_response(aw::RunStopResponse::FAIL);
-		return grpc::Status::OK;
-	}
-	motionEl->setState(BaseAlgorithmElement::RELEASE);
-	response->set_response(aw::RunStopResponse::SUCCESS);
 	return grpc::Status::OK;
 }
 
@@ -109,16 +116,77 @@ grpc::Status AlgorithmGrpcServer::SetMotionRoi(grpc::ServerContext *context, con
 	return grpc::Status::OK;
 }
 
+grpc::Status AlgorithmGrpcServer::RunStabilization(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response)
+{
+	if (stabilizationEl) {
+		stabilizationEl->setState(BaseAlgorithmElement::INIT);
+		response->set_response(aw::RunStopResponse::SUCCESS);
+	} else
+		response->set_response(aw::RunStopResponse::FAIL);
+	return grpc::Status::OK;
+}
+
+grpc::Status AlgorithmGrpcServer::StopStabilization(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response)
+{
+	if (stabilizationEl) {
+		if (stabilizationEl->getState() == BaseAlgorithmElement::STOPALGO)
+			stabilizationEl->setState(BaseAlgorithmElement::PROCESS);
+		else stabilizationEl->setState(BaseAlgorithmElement::STOPALGO);
+		response->set_response(aw::RunStopResponse::SUCCESS);
+	} else
+		response->set_response(aw::RunStopResponse::FAIL);
+	return grpc::Status::OK;
+}
+
+grpc::Status AlgorithmGrpcServer::ReleaseStabilization(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response)
+{
+	if (stabilizationEl) {
+		stabilizationEl->setState(BaseAlgorithmElement::RELEASE);
+		response->set_response(aw::RunStopResponse::SUCCESS);
+	}
+	else
+		response->set_response(aw::RunStopResponse::FAIL);
+	return grpc::Status::OK;
+}
+
+grpc::Status AlgorithmGrpcServer::GetStabilizationState(grpc::ServerContext *context, const aw::Empty *request, aw::AlgoResponse *response)
+{
+	if (!stabilizationEl)
+		return grpc::Status::CANCELLED;
+	aw::AlgoResponse::AlgoState state;
+	state = static_cast<aw::AlgoResponse::AlgoState>(stabilizationEl->getState());
+	response->set_state(state);
+	return grpc::Status::OK;
+}
+
 grpc::Status AlgorithmGrpcServer::SetSensitivity(grpc::ServerContext *context, const aw::Sensitivity *request, aw::GeneralResponse *response)
 {
 	BaseAlgorithmCommon *baseAlgo = BaseAlgorithmCommon::instance();
-	int err = baseAlgo->setSensitivity(request->sensitivity());
+	int err = 0;
+	if (request->type() == aw::Sensitivity::MOTION)
+		err = baseAlgo->setSensitivity("motion_detection", request->sensitivity());
+	else if (request->type() == aw::Sensitivity::STABILIZATION)
+		err = baseAlgo->setSensitivity("stabilization", request->sensitivity());
 	if (err) {
 		response->set_response(aw::GeneralResponse::FAIL);
 		response->set_error_code(err);
 		return grpc::Status::CANCELLED;
 	}
 	response->set_response(aw::GeneralResponse::SUCCESS);
+	return grpc::Status::OK;
+}
+
+grpc::Status AlgorithmGrpcServer::GetSensitivity(grpc::ServerContext *context, const aw::Sensitivity *request, aw::Sensitivity *response)
+{
+	BaseAlgorithmCommon *baseAlgo = BaseAlgorithmCommon::instance();
+	int ret = 0;
+	if (request->type() == aw::Sensitivity::MOTION)
+		ret = baseAlgo->getSensitivity("motion_detection");
+	else if (request->type() == aw::Sensitivity::STABILIZATION)
+		ret = baseAlgo->getSensitivity("stabilization");
+	if (ret < 0)
+		return grpc::Status::CANCELLED;
+	response->set_sensitivity(ret);
 	return grpc::Status::OK;
 }
 
--- algorithm/algorithmgrpcserver.h
+++ algorithm/algorithmgrpcserver.h
@@ -6,25 +6,37 @@
 #include "proto/AlgorithmWorks.grpc.pb.h"
 
 #include "algorithm/motionalgorithmelement.h"
+#include "algorithm/stabilizationalgorithmelement.h"
 
 class GrpcThread;
 class AlgorithmGrpcServer : public aw::AlgorithmWorks::Service
 {
 public:
 	explicit AlgorithmGrpcServer(QObject *parent = 0);
+	// MotionWorks
 	grpc::Status RunMotion(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response) override;
 	grpc::Status StopMotion(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response) override;
 	grpc::Status ReleaseMotion(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response) override;
 	grpc::Status GetMotionState(grpc::ServerContext *context, const aw::Empty *request, aw::AlgoResponse *response) override;
 	grpc::Status SetMotionRoi(grpc::ServerContext *context, const aw::RoiQ *request, aw::GeneralResponse *response) override;
+	// StabilizationWorks
+	grpc::Status RunStabilization(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response) override;
+	grpc::Status StopStabilization(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response) override;
+	grpc::Status ReleaseStabilization(grpc::ServerContext *context, const aw::Empty *request, aw::RunStopResponse *response) override;
+	grpc::Status GetStabilizationState(grpc::ServerContext *context, const aw::Empty *request, aw::AlgoResponse *response) override;
+	// GeneralWorks
 	grpc::Status SetSensitivity(grpc::ServerContext *context, const aw::Sensitivity *request, aw::GeneralResponse *response) override;
+	grpc::Status GetSensitivity(grpc::ServerContext *context, const aw::Sensitivity *request, aw::Sensitivity *response) override;
+
 	void setMotionAlgorithmElement(MotionAlgorithmElement *el) {motionEl = el;}
+	void setStabilizationAlgorithmElement(StabilizationAlgorithmElement *el) {stabilizationEl = el;}
 signals:
 
 public slots:
 protected:
 	GrpcThread *grpcServ;
 	MotionAlgorithmElement *motionEl;
+	StabilizationAlgorithmElement *stabilizationEl;
 };
 
 #endif // ALGORITHMGRPCSERVER_H
